e9446b232b78ce6fe6a25c41b1aeca7cf2e6929b
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2025-05-15 23:01:41 -0700

    Zoho Login Automation Succesful

--- src/test/java/testcase/FirstTest.java
+++ src/test/java/testcase/FirstTest.java
@@ -1,12 +1,33 @@
 package testcase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 public class FirstTest {
 	
 	public static void main(String[] args) {
 		
 		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.zoho.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.className("zgh-login")).click();
+		driver.findElement(By.id("login_id")).sendKeys("arrennbaral@gmail.com");
+		driver.findElement(By.id("nextbtn")).click();
+		driver.findElement(By.id("password")).sendKeys("_________");
+		driver.findElement(By.id("nextbtn")).click();
+	
+		//System.out.println(driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "Zoho Accounts");
+		
+		driver.quit();
+		
+		
+		
+		
+		
 		
 		
 	}
