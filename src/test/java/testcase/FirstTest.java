63a20817dbfe0149569de6d734e6dc4e8bc799ba
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2025-05-15 20:09:49 -0700

    All dependencies installed

---
+++ src/test/java/testcase/FirstTest.java
@@ -0,0 +1,15 @@
+package testcase;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		
+	}
+	
+
+}
