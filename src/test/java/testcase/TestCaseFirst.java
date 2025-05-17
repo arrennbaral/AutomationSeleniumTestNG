cd4f6667effe72c36bb857bff1d7f70066426f09
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2025-05-16 17:59:13 -0700

    Implemented DataProviders to test multiple data

--- src/test/java/testcase/TestCaseFirst.java
+++ src/test/java/testcase/TestCaseFirst.java
@@ -1,5 +1,6 @@
 package testcase;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
@@ -10,17 +11,31 @@ import base.BaseTest;
 
 public class TestCaseFirst extends BaseTest {
 	
-	@Test
-	public static void LoginTest() {
+	@Test(dataProvider = "testdata")
+	public static void LoginTest(String username, String password) {
 		
 		driver.findElement(By.className(loc.getProperty("sign_in"))).click(); //locators -- properties
-		driver.findElement(By.id(loc.getProperty("login_field"))).sendKeys("arrennbaral@gmail.com");
+		driver.findElement(By.id(loc.getProperty("login_field"))).sendKeys(username);
 		driver.findElement(By.id(loc.getProperty("next_button"))).click();
-		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("------");
+		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys(password);
 		driver.findElement(By.id(loc.getProperty("password_field"))).click();
 
 		//System.out.println(driver.getTitle());
 		AssertJUnit.assertEquals(driver.getTitle(), "Zoho Accounts");
 	}
+	
+	@DataProvider(name = "testdata")
+	public Object [][] testData(){
+		
+		return new Object [][] {
+			{"arrennbaral@gmail.com", "2kWip@HgY!S9gAL"},
+			{"arrenngmail.com", "===="},
+			{"arrennbaral@gmaom", "==="},
+			{"arrennbail.com", "=="},
+			{"arrennbil.com", "==="},
+		};
+		
+	}
+	
 
 }
